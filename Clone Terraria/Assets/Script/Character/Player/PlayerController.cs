70e62430f0c5c2ce2db95dc3de44959fbcf0f3b4
Frank <Frank@DESKTOP-S2DM8D4>
2018-12-11 18:21:42 +0800

    UnityCharacter --> PlayerController PlayerController create delegate to control gameObject

---
+++ Clone Terraria/Assets/Script/Character/Player/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Script.Character;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Script.Character
+{
+    public delegate void ComputePhysics();
+    public delegate void Move();
+    public delegate void Interact();
+
+    public class PlayerController : MonoBehaviour
+    {
+        public ComputePhysics computePhysics;
+        public Move move;
+        public Interact interact;
+
+        void FixedUpdate()
+        {
+            computePhysics();         
+        }
+        void Update()
+        {
+            move();
+            interact();
+        }
+    }
+}
