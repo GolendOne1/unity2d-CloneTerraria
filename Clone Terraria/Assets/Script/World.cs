18f5bc4bf18cea56b0aa14cd90acb586e269ecbd
Frank <Frank@DESKTOP-S2DM8D4>
2018-12-11 18:44:10 +0800

    rearrange World.cs

--- Clone Terraria/Assets/Script/World.cs
+++ Clone Terraria/Assets/Script/World.cs
@@ -9,10 +9,7 @@ namespace Assets.Script
     class World : MonoBehaviour
     {
         public Terrain terrain;
-
         public Player     player;
-        //public GameObject playerEntity;
-
         public MainCamera mainCamera;
 
         public void Start()
@@ -22,22 +19,18 @@ namespace Assets.Script
 
             mainCamera = new MainCamera(gameObject ,player.Entity);
         }
+        public void LateUpdate()
+        {
+            mainCamera.computeCamera();
+        }
+
+
         private void createPlayer()
         {
-            //playerEntity = Resources.Load<GameObject>("Prefab/MainCharacter");
-            //playerEntity = Instantiate(playerEntity);
-            //playerEntity.transform.name = "MainCharacter";
-
             List<InteractBehavior> interactBehavior = new List<InteractBehavior>();
             interactBehavior.Add(new Destory(gameObject.GetComponent<Camera>() ,terrain));
 
             player = new Player(interactBehavior);
-            //playerEntity.GetComponent<PlayerController>().mainCharacter = player;
-        }
-
-        void LateUpdate()
-        {
-            mainCamera.computeCamera();
         }
     }
 }
